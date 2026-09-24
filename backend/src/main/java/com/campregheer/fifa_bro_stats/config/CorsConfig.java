b0be0835871321f848d109c05723b42e40def7c9
campregheer <danieljunioor38@gmail.com>
2026-09-24 19:34:21 -0300

    cors allowed prod url

--- backend/src/main/java/com/campregheer/fifa_bro_stats/config/CorsConfig.java
+++ backend/src/main/java/com/campregheer/fifa_bro_stats/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    @Value("${cors.allowed-origins:http://localhost:5173}")
+    @Value("${cors.allowed-origins:https://fifa-bro-stats.vercel.app/}")
     private String[] allowedOrigins;
 
     @Bean
